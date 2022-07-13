c604a85591558c54464f77829f8368fcb3ab64ae
letrong29 <letrong2929Gmail.com>
2022-07-13 14:04:39 +0700

    first commit

--- ss1. database/exercise/excercise_xây_dựng_CSDL_quản_lý_sv.sql
+++ ss1. database/exercise/excercise_xây_dựng_CSDL_quản_lý_sv.sql
@@ -1,22 +1,26 @@
 
-create table student_management.class(
-id int auto_increment,
-name_class varchar(45),
-primary key(id)
+CREATE TABLE student_management.class (
+    id INT AUTO_INCREMENT,
+    name_class VARCHAR(45),
+    PRIMARY KEY (id)
 );
 
-create table student_management.teacher(
-id int auto_increment,
-name_teacher varchar(45),
-age int,
-country varchar(45),
-primary key(id)
+CREATE TABLE student_management.teacher (
+    id INT AUTO_INCREMENT,
+    name_teacher VARCHAR(45),
+    age INT,
+    country VARCHAR(45),
+    PRIMARY KEY (id)
 );
 
-insert into class(name_class) value ("C0422G1"),("C0522G1");
+insert into class(name_class) 
+value ("C0422G1"),("C0522G1");
 
-insert into teacher(name_teacher, age, country) value ("HaiTT", 24, "da nang"), ("TienNVT", 27, "da nang");
+insert into teacher(name_teacher, age, country) 
+value ("HaiTT", 24, "da nang"), ("TienNVT", 27, "da nang");
 
-update class set class.name_class = "C0322G1" where id = 1;
+update class 
+set class.name_class = "C0322G1" where id = 1;
 
-update class set class.name_class = "C0422G1" where id = 1;
+update class 
+set class.name_class = "C0422G1" where id = 1;
